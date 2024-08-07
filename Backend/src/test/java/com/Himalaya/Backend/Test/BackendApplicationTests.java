616636e665f1cef3c064c9082f20dc50d435e9b2
DESKTOP-TB3I824\admin <sainaath4763@gmail.com>
2024-08-08 00:26:01 +0530

    test class added

---
+++ Backend/src/test/java/com/Himalaya/Backend/Test/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Himalaya.Backend.Test;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
